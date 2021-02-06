75f853956e3694d530931bff6810468b334040be
ducktrshessami <jwee761@gmail.com>
2021-02-06 11:42:44 -0800

    post /api/workouts creates workout entry

--- routes/api.js
+++ routes/api.js
@@ -34,7 +34,12 @@ module.exports = function(app) {
 
     // createWorkout
     app.post(endpoint, function(req, res) {
-
+        db.Workout.create(req.body)
+            .then(data => res.status(200).json(data))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
     });
 
     // getWorkoutsInRange
