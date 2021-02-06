3dbd112a9ec1cda57d350b34e77fa1fab16ade67
ducktrshessami <jwee761@gmail.com>
2021-02-06 11:55:47 -0800

    use native find and modify

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ mongoose.connection.once("open", function() {
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
