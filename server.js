c6e008aaa59ab723b4141d954936f8b7db4c08c4
Josh Fish <jwee761@gmail.com>
2021-02-05 17:18:16 -0800

    use morgan for express logging and connect to mongo

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 require("./routes")(app);
 
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+mongoose.once("open", function() {
+    console.log("Connected to the database");
+    app.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}`);
+    });
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
