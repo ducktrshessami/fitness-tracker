c4adfb6dc02b6c3fc4ad2191269b368cc5a6b2e9
ducktrshessami <jwee761@gmail.com>
2021-02-06 11:35:22 -0800

    add totalDuration prop to all workouts

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,19 @@ const db = require("../models");
 
 const endpoint = "/api/workouts";
 
+// Helper function used with Array.map
+function mapWithTotalDuration(workout) {
+    let foo = JSON.parse(JSON.stringify(workout));
+    foo.totalDuration = foo.exercises.reduce((a, b) => a + b.duration, 0);
+    return foo;
+}
+
 module.exports = function(app) {
     // getLastWorkout
     app.get(endpoint, function(req, res) {
         db.Workout.find()
             .sort("day")
+            .then(data => data.map(mapWithTotalDuration))
             .then(data => res.status(200).json(data))
             .catch(err => {
                 console.error(err);
