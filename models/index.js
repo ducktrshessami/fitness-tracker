bc7d84504eb47f92ab3d9e3859be1024baa2e0bc
Josh Fish <jwee761@gmail.com>
2021-02-05 16:40:43 -0800

    models index creates models from assumed schema objects

--- models/index.js
+++ models/index.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const basename = path.basename(__filename);
 
-module.exports = function(app) {
-    fs
-        .readdirSync(__dirname)
-        .filter(file => {
-            return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-        })
-        .forEach(file => require(path.join(__dirname, file))(app));
-};
+var db = {};
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        let modelName = file.slice(0, -3);
+        db[modelName] = mongoose.model(modelName, new mongoose.Schema(require(path.join(__dirname, file))));
+    });
+
+module.exports = db;
