a183cbad163197245e4ee1e7c7ee08490f67bd54
ducktrshessami <jwee761@gmail.com>
2021-02-06 11:43:50 -0800

    the three pages send their html

--- routes/html.js
+++ routes/html.js
@@ -4,14 +4,14 @@ const public = path.resolve(__dirname, "..", "public");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        
+        res.status(200).sendFile(path.join(public, "index.html"));
     });
 
     app.get("/exercise", function(req, res) {
-        
+        res.status(200).sendFile(path.join(public, "exercise.html"));
     });
 
     app.get("/stats", function(req, res) {
-
+        res.status(200).sendFile(path.join(public, "stats.html"));
     });
 };
